6543268ea563248e3f6c9a52d47f0872adb365c3
Clark Wood <wood@linprog4.cs.fsu.edu>
2013-09-18 10:35:14 -0400

    added parsing for target rules

--- proj2/clark_make.h
+++ proj2/clark_make.h
@@ -6,29 +6,46 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define kStringLength 256
 #define kNumRules 16
+#define kStringLength 256
 #define maxRecursiveDepth 32
 
 #define DEBUG printf("HERE %d\n", __LINE__);fflush(stdout);
 #define DBGING 1
 
-// struct holding makefile rules
+// Struct holding makefile rules.
+// Each char char used like a key:value pair.
+// Macros are arrays of strings, rules arrays of string arrays, 
+// since one key maps to a list of instructions.
+// Rules must also know how many commands are in each rule
 typedef struct {
-    // 16 64 char length rules
-    int numMacroRules;
-    char macroKey[kNumRules][kStringLength];    
-    char macroValue[kNumRules][kStringLength];    
+    int numMacros;
+    char macroKeys[kNumRules][kStringLength];    
+    char macroValues[kNumRules][kStringLength];    
 
-    // Prevent infinite recursion
-    int recursiveDepth;
+    int numTargetRules;
+    char targetKeys[kNumRules][kStringLength];    
+    int numCommandsInTargetKey[kNumRules];
+    char targetValues[kNumRules][kNumRules][kStringLength];    
+
+    int numInferencesRules;
+    char inferenceKeys[kNumRules][kStringLength];    
+    int numCommandsInReferenceKey[kNumRules];
+    char inferenceValues[kNumRules][kNumRules][kStringLength];    
+
 
 } rulesStruct;
 
 // Make a rulesStruct and init properly
 rulesStruct rulesFactory(rulesStruct rules) {
-    rules.numMacroRules = 0;
-    rules.recursiveDepth = 0;
+    // Should start with 0 macros/rules
+    rules.numMacros = 0;
+    rules.numTargetRules = 0;
+    rules.numInferencesRules = 0;
+
+    // Each rule should start with 0 commands
+    memset(rules.numCommandsInTargetKey, 0, kNumRules);
+    memset(rules.numCommandsInReferenceKey, 0, kNumRules);
 
     return rules;
 }
@@ -37,7 +54,7 @@ rulesStruct rulesFactory(rulesStruct rules) {
 // 
 rulesStruct parseMakefile(FILE* makefile, rulesStruct rules)
 {
-    rules.numMacroRules = 0;
+    rules.numMacros = 0;
     char str[kStringLength];
     while (fgets(str, kStringLength, makefile) ) {
        
@@ -55,29 +72,68 @@ rulesStruct parseMakefile(FILE* makefile, rulesStruct rules)
             case '.':
                 break;
 
-            // Macro
+            // Target rule or Macro
             default:
-                ret = sscanf(str, "%[^=]%*c%s\n", 
-                                rules.macroKey[rules.numMacroRules], 
-                                rules.macroValue[rules.numMacroRules]);
 
-                // Find 2 strings? Add the rule
-                if (ret == 2) {
-                    rules.numMacroRules++;
-                }   
+                // No '.' but has ':', then target rule
+                if (strchr(str, ':') != NULL) {
+                    // Get target0,1,2... : prereq0,1,2,...
+                    strcpy(rules.targetKeys[rules.numTargetRules], str);
+                    
+                    // Now get rules
+                    char c = getc(makefile);
+                    while (c == '\t') {
+                        fgets(rules.targetValues[rules.numTargetRules] 
+                                                [rules.numCommandsInTargetKey[rules.numTargetRules]],
+                              kStringLength, 
+                              makefile);
+                        rules.numCommandsInTargetKey[rules.numTargetRules]++;
+                        //fgets(str, kStringLength, makefile);
+                        //strcpy(rules.targetValues[rules.numCommandsInTargetKey[rules.numTargetRules]], str);
+                        c = getc(makefile);
+                    }
+
+                    // Put last char back, it isn't tab so it's the start of the next rule
+                    ungetc(c, makefile);
+
+                    // Add rule
+                    rules.numTargetRules++;
+                }
+
+                // Has '=', then macro
+                else if (strchr(str, '=') != NULL) {
+                    ret = sscanf(str, "%[^=]%*c%s\n", 
+                            rules.macroKeys[rules.numMacros], 
+                            rules.macroValues[rules.numMacros]);
+
+                    // Find 2 strings? Macro syntax correct, add the rule
+                    if (ret == 2) {
+                        rules.numMacros++;
+                    }   
+                }
 
         }
 
     }
 
-    #ifdef DBGING
-    for (int i = 0; i < rules.numMacroRules; i++) {
-        printf("Rule %d: %s = %s\n", i, rules.macroKey[i], rules.macroValue[i]);
+#ifdef DBGING
+    printf("Macros:\n");
+    for (int i = 0; i < rules.numMacros; i++) {
+        printf("Macro %d: %s = %s\n", i, rules.macroKeys[i], rules.macroValues[i]);
     }
-    #endif
+    printf("Target Rules:\n");
+    for (int i = 0; i < rules.numTargetRules; i++) {
+        printf("Target %d: %s", i, rules.targetKeys[i]);
+
+        for (int j = 0; j < rules.numCommandsInTargetKey[i]; j++) {
+            printf("\t%s", rules.targetValues[i][j]);
+        }
+    }
+#endif
 
 
     return rules;
+
 } // end parseMakefileRules
 
 // Recursively resolve key:value pairs of macros
@@ -85,13 +141,13 @@ char* resolveMacro(rulesStruct rules, char* key, int recursiveDepth)
 {
     // likely we have a loop
     if (recursiveDepth == maxRecursiveDepth) {
-        printf("Probably encountered a loop\n");
+        printf("Probably encountered a circular macro definition\n");
         return (char*)0;
     }
 
-    for (int i = 0; i < rules.numMacroRules; i++) {
-        if (strcmp(rules.macroKey[i], key) == 0) {
-            return resolveMacro(rules, rules.macroValue[i], recursiveDepth + 1);
+    for (int i = 0; i < rules.numMacros; i++) {
+        if (strcmp(rules.macroKeys[i], key) == 0) {
+            return resolveMacro(rules, rules.macroValues[i], recursiveDepth + 1);
         }
     }
 
