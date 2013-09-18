afdb6b3a5ef8b8a62bf1a07deb196244f75917df
Clark Wood <wood@linprog4.cs.fsu.edu>
2013-09-18 11:19:18 -0400

    tokenizing inference rules done too, should be refactored with target rules into getRules function

--- proj2/clark_make.h
+++ proj2/clark_make.h
@@ -28,9 +28,9 @@ typedef struct {
     int numCommandsInTargetKey[kNumRules];
     char targetValues[kNumRules][kNumRules][kStringLength];    
 
-    int numInferencesRules;
+    int numInferenceRules;
     char inferenceKeys[kNumRules][kStringLength];    
-    int numCommandsInReferenceKey[kNumRules];
+    int numCommandsInInferenceKey[kNumRules];
     char inferenceValues[kNumRules][kNumRules][kStringLength];    
 
 
@@ -41,14 +41,47 @@ rulesStruct rulesFactory(rulesStruct rules) {
     // Should start with 0 macros/rules
     rules.numMacros = 0;
     rules.numTargetRules = 0;
-    rules.numInferencesRules = 0;
+    rules.numInferenceRules = 0;
 
     // Each rule should start with 0 commands
     memset(rules.numCommandsInTargetKey, 0, kNumRules);
-    memset(rules.numCommandsInReferenceKey, 0, kNumRules);
+    memset(rules.numCommandsInInferenceKey, 0, kNumRules);
 
     return rules;
 }
+
+// Generic function to get Target or Inference rules
+/*
+void getRules(FILE* makefile, 
+              char* str[kStringLength],
+              int* numRules, 
+              char* keys[kNumRules][kStringLength],
+              int* numCommandsInKey[kNumRules],
+              char* values[kNumRules][kNumRules][kStringLength])
+{
+    // Get target0,1,2... : prereq0,1,2,...
+    strcpy(keys[(*numRules)], str);
+
+    // Now get rules
+    char c = getc(makefile);
+    while (c == '\t') {
+        fgets((*values)[(*numRules)] 
+                [(*numCommandsInKey)[(*numRules)]],
+                kStringLength, 
+                makefile);
+        (*numCommandsInKey)[(*numRules)]++;
+        //fgets(str, kStringLength, makefile);
+        //strcpy(rules.targetValues[rules.numCommandsInTargetKey[rules.numTargetRules]], str);
+        c = getc(makefile);
+    }
+    // Put last char back, it isn't tab so it's the start of the next rule
+    ungetc(c, makefile);
+
+    // Add rule
+    (*numRules)++;
+
+} // end getRules
+*/
 
 
 // 
@@ -57,19 +90,42 @@ rulesStruct parseMakefile(FILE* makefile, rulesStruct rules)
     rules.numMacros = 0;
     char str[kStringLength];
     while (fgets(str, kStringLength, makefile) ) {
-       
+
         int ret;
         switch(str[0]) {
             // Blank line
             case '\n':
                 break;
 
-            // Comment line
+                // Comment line
             case '#':
                 break;
 
-            // Inference rule
+                // Inference rule
             case '.':
+                // TODO factor this and target case into getRules()
+                // Get target0,1,2... : prereq0,1,2,...
+                strcpy(rules.inferenceKeys[rules.numInferenceRules], str);
+
+                // Now get rules
+                char c = getc(makefile);
+                while (c == '\t') {
+                    fgets(rules.inferenceValues[rules.numInferenceRules] 
+                            [rules.numCommandsInInferenceKey[rules.numInferenceRules]],
+                            kStringLength, 
+                            makefile);
+                    rules.numCommandsInInferenceKey[rules.numInferenceRules]++;
+                    //fgets(str, kStringLength, makefile);
+                    //strcpy(rules.inferenceValues[rules.numCommandsInInferenceKey[rules.numInferenceRules]], str);
+                    c = getc(makefile);
+                }
+
+                // Put last char back, it isn't tab so it's the start of the next rule
+                ungetc(c, makefile);
+
+                // Add rule
+                rules.numInferenceRules++;
+
                 break;
 
             // Target rule or Macro
@@ -79,14 +135,14 @@ rulesStruct parseMakefile(FILE* makefile, rulesStruct rules)
                 if (strchr(str, ':') != NULL) {
                     // Get target0,1,2... : prereq0,1,2,...
                     strcpy(rules.targetKeys[rules.numTargetRules], str);
-                    
+
                     // Now get rules
                     char c = getc(makefile);
                     while (c == '\t') {
                         fgets(rules.targetValues[rules.numTargetRules] 
-                                                [rules.numCommandsInTargetKey[rules.numTargetRules]],
-                              kStringLength, 
-                              makefile);
+                                [rules.numCommandsInTargetKey[rules.numTargetRules]],
+                                kStringLength, 
+                                makefile);
                         rules.numCommandsInTargetKey[rules.numTargetRules]++;
                         //fgets(str, kStringLength, makefile);
                         //strcpy(rules.targetValues[rules.numCommandsInTargetKey[rules.numTargetRules]], str);
@@ -121,12 +177,22 @@ rulesStruct parseMakefile(FILE* makefile, rulesStruct rules)
     for (int i = 0; i < rules.numMacros; i++) {
         printf("Macro %d: %s = %s\n", i, rules.macroKeys[i], rules.macroValues[i]);
     }
+
     printf("Target Rules:\n");
     for (int i = 0; i < rules.numTargetRules; i++) {
         printf("Target %d: %s", i, rules.targetKeys[i]);
 
         for (int j = 0; j < rules.numCommandsInTargetKey[i]; j++) {
             printf("\t%s", rules.targetValues[i][j]);
+        }
+    }
+
+    printf("Inference Rules:\n");
+    for (int i = 0; i < rules.numInferenceRules; i++) {
+        printf("Inference %d: %s", i, rules.inferenceKeys[i]);
+
+        for (int j = 0; j < rules.numCommandsInInferenceKey[i]; j++) {
+            printf("\t%s", rules.inferenceValues[i][j]);
         }
     }
 #endif
