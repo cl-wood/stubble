1b99d32ee4ac4570a755cf3f5bf0f343e03a563d
cwood <clark.w.wood@gmail.com>
2013-11-10 13:58:12 -0500

    have loss and win case, need to drive mutation correctly

--- MyPinTool/driver.py
+++ MyPinTool/driver.py
@@ -1,40 +1,74 @@
 # Driver to decide how to mutate
 
-import os, subprocess
+import os, shutil
+import subprocess
 from operator import itemgetter
 
-def main():
+
+def test(inputFile, outputFile, seed = ('0', '0', '0') ):
+
+    mutate(inputFile, seed)
 
     cmd =   [   
         '../pin/pin', 
         '-t', 'obj-ia32/MyPinTool.so', 
-        '--', '../vulnerable_programs/trivial/a.out', 'file.txt'
+        '--', '../vulnerable_programs/trivial/a.out', inputFile
     ]
 
-    # One iteration of pintool, generates new (file.txt, results.out) pair
+    testNumber = getTestNumber('./results/')
+    print('Running test number', testNumber, 'based on seed', seed, '...')
+
+    # One iteration of pintool, generates new (input.txt, results.out) pair
     p1 = subprocess.Popen(cmd)
     p1.wait()
 
-    # Get set diffs to decide which parts of user input to mutate next
-    explored_results_set = open('./results/explored.out', 'r').read().split()
-    #while set(explored_results_set) - 
-    results = open('./results.out', 'r').read().split()
-    newResults = set(results) - set(explored_results_set) 
+    # Find which results are new
+    # input is copied to input/ and mutated
+    # results is copied to results/
+    explored = open('./results/explored.0', 'r').read().split()
+    exploredFile = open('./results/explored.0', 'a')
+    results = open(outputFile, 'r').read().split()
+    newResults = set(results) - set(explored) 
+
+    # Copy results and append latest results to explored
+    copyResults(inputFile, newResults, testNumber) 
+    exploredFile.write(':'.join(seed) + "\n")
 
     # file in format pos:op:arg1:arg2
     toExplore = [r.split(':') for r in newResults]
 
     toExplore = sorted(toExplore, key=itemgetter(0))
-    print toExplore
+    return toExplore[0]
 
     # explore each 
     #for i in toExplore:
-    # End main()
 
-def moveOperations(inputFile, outputFile):
-    results = os.listdir('.')
+# End main()
 
-def getMutationNumber(resultsDir):
+# TODO will need to handle non-character cases for seed[1]
+def mutate(inputFile, seed):
+    f = list(open(inputFile, 'r').read())
+
+    if (int(seed[1]) < 256):
+        f[int(seed[0])] = chr(int(seed[1]))
+
+    out = ''.join(f)
+
+    open(inputFile, 'w').write(out)
+
+# End mutate
+
+def copyResults(inputFile, outputSet, testNumber):
+
+    shutil.copy(inputFile, './input/input.' + str(testNumber) )
+    #shutil.copy(outputFile, './results/results.' + str(testNumber) )
+    results = open('./results/results.' + str(testNumber), 'w' )
+    for i in outputSet:
+        results.write(i + "\n")
+
+# End copyOperations()
+
+def getTestNumber(resultsDir):
 
     results = os.listdir(resultsDir)
     if len(results) == 1:
@@ -43,13 +77,17 @@ def getMutationNumber(resultsDir):
     return max([int(i.split('.')[1]) for i in results]) + 1
 
     
-# End getMutationsNumber
+# End getTestNumber
 
 
+i = './file.txt'
+o = './results.out'
 
+newSeed = test(i, o)
+win = ('300', '88', '88')
+test(i, o, win)
 
-#main()
-#moveOperations('file.txt', 'results.out')
+#while set(explored_results_set) - 
 
 
 
