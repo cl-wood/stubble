81a0f55f89d9e24d342642c2e4ae15437ed3d83f
clark wood <clark.w.wood@gmail.com>
2014-03-22 13:36:13 -0400

    paper work and idea for strcpy

---
+++ MyPinToolStrcpy/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+rm -f input/input.* output/output.* taint/taint.* mutations/mutation.*
+rm -f results.out file.out taint.out
+rm -f buffer.out.*
+cp fileLose.txt inputFile.txt
+echo > taint/explored.0
+
